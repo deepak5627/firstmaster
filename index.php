780478dc19355099670c28da239f4d0095a0008b
deepak5627 <30997503+deepak5627@users.noreply.github.com>
2020-04-16 18:30:04 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,9 @@
 <!DOCTYPE html>
 <html>
-	<head>
-
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-
-	<title>Dynamic Add squares usng jquery</title>
-	<style type="text/css">
+<head>
+	<title></title>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<style type="text/css">
 		.outer{
 			
 			height: 350px;
@@ -22,33 +20,49 @@
 			background-color:green; 
 		}
 	</style>
-	</head>
-	<body>
+</head>
+<body>
+	<form>
+		<input type="text" name="row" class="row">
+		<input type="text" name="column" class="column">
 
-		<div class="outer">
-			
-		</div>
-		<button  type="button" id="btn">Add</button>
+		<button type="button" name="button" id="button" class="button">Show Square</button>
 		<button  type="button" id="btn_rmv">Remove</button>
-	</body>
+	</form>
+	<br>
+	<br>
+	<br>
+		<div class="outer"></div>
+</body>
 </html>
 <script type="text/javascript">
 	$(document).ready(function(){
-		$('#btn').click(function(){
-			var element = $("<div class='inner'></div>");
-			element.appendTo('.outer');
-		});
+	
+		$('#button').click(function(){
+			$( ".outer" ).empty();
 
-	$('.outer').on('click','.inner',function(e){
+			var row1 = $( ".row" ).val();
+			console.log(row1);
+			var row = $( ".row" ).val();
+			var col = $( ".column" ).val();
+
+			var squares = row * col ; 
+			for(var i=1;i<=squares;i++){
+				var element = $("<div class='inner'></div>");
+				element.appendTo('.outer');	
+			}
+			
+		});
+		$('.outer').on('click','.inner',function(e){
 		e.preventDefault();
 		    $(this).toggleClass('greenColor');
 			// $(this).css({"background-color": "green"});
 		})
 
+		
 	$('#btn_rmv').on("click", function(){
 	    $('.outer .inner:last').remove();
 	});
-
 	});
-</script>
 
+</script>
